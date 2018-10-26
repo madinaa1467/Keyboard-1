bb89ea08827088cee6cce0e040d42ea80c094478
msultanova5 <msultanova5@greet-go.com>
2018-10-26 19:06:34 +0600

    calculate soft menu bar

--- src/android/IonicKeyboard.java
+++ src/android/IonicKeyboard.java
@@ -1,74 +1,66 @@
 package io.ionic.keyboard;
 
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaInterface;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.PluginResult;
+import android.content.Context;
+import android.graphics.Rect;
+import android.os.Build;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
+import android.view.inputmethod.InputMethodManager;
+import org.apache.cordova.*;
 import org.apache.cordova.PluginResult.Status;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import android.content.Context;
-import android.graphics.Rect;
-import android.view.View;
-import android.view.ViewTreeObserver.OnGlobalLayoutListener;
-import android.view.inputmethod.InputMethodManager;
-// import additionally required classes for calculating screen height
-import android.view.Display;
-import android.graphics.Point;
-import android.os.Build;
-import android.util.DisplayMetrics;
-import android.view.WindowManager;
-
 import static android.os.Build.VERSION.SDK_INT;
-import static android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1;
 import static android.os.Build.VERSION_CODES.JELLY_BEAN_MR1;
+
+// import additionally required classes for calculating screen height
 
 public class IonicKeyboard extends CordovaPlugin {
 
-    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-        super.initialize(cordova, webView);
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    super.initialize(cordova, webView);
+  }
+
+  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+    if ("close".equals(action)) {
+      cordova.getThreadPool().execute(new Runnable() {
+        public void run() {
+          //http://stackoverflow.com/a/7696791/1091751
+          InputMethodManager inputManager = (InputMethodManager) cordova.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+          View v = cordova.getActivity().getCurrentFocus();
+
+          if (v == null) {
+            callbackContext.error("No current focus");
+          } else {
+            inputManager.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+            callbackContext.success(); // Thread-safe.
+          }
+        }
+      });
+      return true;
     }
-
-    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        if ("close".equals(action)) {
-            cordova.getThreadPool().execute(new Runnable() {
-                public void run() {
-                    //http://stackoverflow.com/a/7696791/1091751
-                    InputMethodManager inputManager = (InputMethodManager) cordova.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-                    View v = cordova.getActivity().getCurrentFocus();
-
-                    if (v == null) {
-                        callbackContext.error("No current focus");
-                    } else {
-                        inputManager.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-                        callbackContext.success(); // Thread-safe.
-                    }
-                }
-            });
-            return true;
+    if ("show".equals(action)) {
+      cordova.getThreadPool().execute(new Runnable() {
+        public void run() {
+          ((InputMethodManager) cordova.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE)).toggleSoftInput(0, InputMethodManager.HIDE_IMPLICIT_ONLY);
+          callbackContext.success(); // Thread-safe.
         }
-        if ("show".equals(action)) {
-            cordova.getThreadPool().execute(new Runnable() {
-                public void run() {
-                    ((InputMethodManager) cordova.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE)).toggleSoftInput(0, InputMethodManager.HIDE_IMPLICIT_ONLY);
-                    callbackContext.success(); // Thread-safe.
-                }
-            });
-            return true;
-        }
-        if ("init".equals(action)) {
-            cordova.getThreadPool().execute(new Runnable() {
-                public void run() {
-                    //calculate density-independent pixels (dp)
-                    //http://developer.android.com/guide/practices/screens_support.html
-                    DisplayMetrics dm = new DisplayMetrics();
-                    cordova.getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
-                    final float density = dm.density;
+      });
+      return true;
+    }
+    if ("init".equals(action)) {
+      cordova.getThreadPool().execute(new Runnable() {
+        public void run() {
+          //calculate density-independent pixels (dp)
+          //http://developer.android.com/guide/practices/screens_support.html
+          DisplayMetrics dm = new DisplayMetrics();
+          cordova.getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
+          final float density = dm.density;
 
-                    //http://stackoverflow.com/a/4737265/1091751 detect if keyboard is showing
-                    final View rootView = cordova.getActivity().getWindow().getDecorView().findViewById(android.R.id.content).getRootView();
+          //http://stackoverflow.com/a/4737265/1091751 detect if keyboard is showing
+          final View rootView = cordova.getActivity().getWindow().getDecorView().findViewById(android.R.id.content).getRootView();
 ////                            Rect r = new Rect();
 ////                            //r will be populated with the coordinates of your view that area still visible.
 ////                            rootView.getWindowVisibleDisplayFrame(r);
@@ -89,18 +81,19 @@ public class IonicKeyboard extends CordovaPlugin {
 ////                                    int StatusBarHeight = 0;
 ////                            }
 //
-                    OnGlobalLayoutListener list = new OnGlobalLayoutListener() {
-                        int previousHeightDiff = 0;
-                        int statusBarHeight = 0;
-                        @Override
-                        public void onGlobalLayout() {
-                            Rect r = new Rect();
-                            //r will be populated with the coordinates of your view that area still visible.
-                            rootView.getWindowVisibleDisplayFrame(r);
+          OnGlobalLayoutListener list = new OnGlobalLayoutListener() {
+            int previousHeightDiff = 0;
+            int statusBarHeight = 0;
 
-                            PluginResult result;
+            @Override
+            public void onGlobalLayout() {
+              Rect r = new Rect();
+              //r will be populated with the coordinates of your view that area still visible.
+              rootView.getWindowVisibleDisplayFrame(r);
 
-                            // cache properties for later use
+              PluginResult result;
+
+              // cache properties for later use
 //                            int rootViewHeight = rootView.getRootView().getHeight();
 //                            int resultBottom = r.bottom;
 //
@@ -118,49 +111,49 @@ public class IonicKeyboard extends CordovaPlugin {
 //                            }
 //                            int heightDiff = screenHeight - resultBottom;
 
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                                DisplayMetrics metrics = new DisplayMetrics();
-                                cordova.getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
-                                int usableHeight = metrics.heightPixels;
-                                cordova.getActivity().getWindowManager().getDefaultDisplay().getRealMetrics(metrics);
-                                int realHeight = metrics.heightPixels;
-                                if (realHeight > usableHeight) { statusBarHeight = realHeight - usableHeight;}
-                                else { statusBarHeight = 0;}
-                            }
-
-                            int heightDiff = rootView.getRootView().getHeight() - r.bottom;
-
-                            int pixelHeightDiff = (int)((heightDiff - statusBarHeight) / density);
-//                            int pixelStatusBarHeight = (int)(statusBarHeight / density);
-                            if (pixelHeightDiff > 100 && pixelHeightDiff != previousHeightDiff) { // if more than 100 pixels, its probably a keyboard...
-                                String msg = "S" + Integer.toString(pixelHeightDiff);
-                                result = new PluginResult(PluginResult.Status.OK, msg);
-                                result.setKeepCallback(true);
-                                callbackContext.sendPluginResult(result);
-                            }
-                            else if ( pixelHeightDiff != previousHeightDiff && ( previousHeightDiff - pixelHeightDiff ) > 100 ){
-                                String msg = "H";
-                                result = new PluginResult(PluginResult.Status.OK, msg);
-                                result.setKeepCallback(true);
-                                callbackContext.sendPluginResult(result);
-                            }
-                            previousHeightDiff = pixelHeightDiff;
-                        }
-                    };
-
-
-                    rootView.getViewTreeObserver().addOnGlobalLayoutListener(list);
-
-
-                    PluginResult dataResult = new PluginResult(PluginResult.Status.OK);
-                    dataResult.setKeepCallback(true);
-                    callbackContext.sendPluginResult(dataResult);
+              if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                DisplayMetrics metrics = new DisplayMetrics();
+                cordova.getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
+                int usableHeight = metrics.heightPixels;
+                cordova.getActivity().getWindowManager().getDefaultDisplay().getRealMetrics(metrics);
+                int realHeight = metrics.heightPixels;
+                if (realHeight > usableHeight) { statusBarHeight = realHeight - usableHeight;} else {
+                  statusBarHeight = 0;
                 }
-            });
-            return true;
+              }
+
+              int heightDiff = rootView.getRootView().getHeight() - r.bottom;
+
+              int pixelHeightDiff = (int) (heightDiff / density);
+//                            int pixelStatusBarHeight = (int)(statusBarHeight / density);
+              if (pixelHeightDiff > 100 && pixelHeightDiff != previousHeightDiff) { // if more than 100 pixels, its probably a keyboard...
+                String msg = "S" + Integer.toString(pixelHeightDiff - statusBarHeight);
+                result = new PluginResult(PluginResult.Status.OK, msg);
+                result.setKeepCallback(true);
+                callbackContext.sendPluginResult(result);
+              } else if (pixelHeightDiff != previousHeightDiff && (previousHeightDiff - pixelHeightDiff) > 100) {
+                String msg = "H";
+                result = new PluginResult(PluginResult.Status.OK, msg);
+                result.setKeepCallback(true);
+                callbackContext.sendPluginResult(result);
+              }
+              previousHeightDiff = pixelHeightDiff;
+            }
+          };
+
+
+          rootView.getViewTreeObserver().addOnGlobalLayoutListener(list);
+
+
+          PluginResult dataResult = new PluginResult(PluginResult.Status.OK);
+          dataResult.setKeepCallback(true);
+          callbackContext.sendPluginResult(dataResult);
         }
-        return false;  // Returning false results in a "MethodNotFound" error.
+      });
+      return true;
     }
+    return false;  // Returning false results in a "MethodNotFound" error.
+  }
 
 
 }
