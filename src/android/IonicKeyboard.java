98c861bb302f402f5709a284e2f2048c349c5cd7
msultanova5 <msultanova5@greet-go.com>
2018-11-08 15:29:38 +0600

    проверка на телефон без нав бара

--- src/android/IonicKeyboard.java
+++ src/android/IonicKeyboard.java
@@ -140,7 +140,7 @@ public class IonicKeyboard extends CordovaPlugin {
 
                 boolean fullScreen = (cordova.getActivity().getWindow().getAttributes().flags & WindowManager.LayoutParams.FLAG_FULLSCREEN) != 0;
                 if (fullScreen) {
-                  screenHeight = rootViewHeight - navBarHeight;
+                  screenHeight = rootViewHeight;
                 }
               } else {
                 screenHeight = rootViewHeight;
