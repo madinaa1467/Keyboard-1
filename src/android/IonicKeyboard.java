f28f3f00e5b29d9c7dc94918546f9efa47a15295
msultanova5 <msultanova5@greet-go.com>
2018-11-09 15:01:24 +0600

    проверка на телефон без нав бара

--- src/android/IonicKeyboard.java
+++ src/android/IonicKeyboard.java
@@ -137,7 +137,13 @@ public class IonicKeyboard extends CordovaPlugin {
 
               System.out.println("dfkjdgfkkjdgfkdgfskjdgfs sdfdgfskjdgfs================================================== hasBackKey=" + hasBackKey);
               System.out.println("dfkjdgfkkjdgfkdgfskjdgfs sdfdgfskjdgfs================================================== hasHomeKey=" + hasHomeKey);
-              if (!(hasBackKey && hasHomeKey) && View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
+              System.out.println("dfkjdgfkkjdgfkdgfskjdgfs sdfdgfskjdgfs================================================== View.SYSTEM_UI_FLAG_FULLSCREEN =" + View.SYSTEM_UI_FLAG_FULLSCREEN);
+              if (!(hasBackKey && hasHomeKey) && (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == 0) {
                 screenHeight = rootViewHeight;
               } else {
 
