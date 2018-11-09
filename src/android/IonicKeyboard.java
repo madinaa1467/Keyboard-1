aaf6fa7ae489e656bf646d95979ebc7bd3780981
msultanova5 <msultanova5@greet-go.com>
2018-11-09 15:19:32 +0600

    проверка на телефон без нав бара

--- src/android/IonicKeyboard.java
+++ src/android/IonicKeyboard.java
@@ -138,17 +138,17 @@ public class IonicKeyboard extends CordovaPlugin {
               System.out.println("dfkjdgfkkjdgfkdgfskjdgfs sdfdgfskjdgfs================================================== hasBackKey=" + hasBackKey);
               System.out.println("dfkjdgfkkjdgfkdgfskjdgfs sdfdgfskjdgfs================================================== hasHomeKey=" + hasHomeKey);
               System.out.println("dfkjdgfkkjdgfkdgfskjdgfs sdfdgfskjdgfs================================================== View.SYSTEM_UI_FLAG_FULLSCREEN =" + (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY));
+                || View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                || View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                || View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                || View.SYSTEM_UI_FLAG_FULLSCREEN
+                || View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY));
               if (!(hasBackKey && hasHomeKey) || (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == 1) {
+                || View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                || View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                || View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                || View.SYSTEM_UI_FLAG_FULLSCREEN
+                || View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == 1) {
                 screenHeight = rootViewHeight;
               } else {
 
