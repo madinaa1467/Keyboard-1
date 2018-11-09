9cbcc9a79553dfaf570b1051f8ff63c61771c437
msultanova5 <msultanova5@greet-go.com>
2018-11-09 15:43:51 +0600

    проверка на телефон без нав бара

--- src/android/IonicKeyboard.java
+++ src/android/IonicKeyboard.java
@@ -145,7 +145,7 @@ public class IonicKeyboard extends CordovaPlugin {
 //                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 //                | View.SYSTEM_UI_FLAG_FULLSCREEN
 //                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
-                cordova.getActivity().getWindow().getDecorView().getSystemUiVisibility() == 1) {
+                (cordova.getActivity().getWindow().getDecorView().getSystemUiVisibility() | View.SYSTEM_UI_FLAG_FULLSCREEN |  View.SYSTEM_UI_FLAG_LAYOUT_STABLE) == 1) {
                 screenHeight = rootViewHeight;//
               } else {
 
